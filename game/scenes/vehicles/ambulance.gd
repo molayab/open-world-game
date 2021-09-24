ff8590db228e7017230d91b65aabe17553dbdd4c
Mateo Olaya <mateo.olaya@globant.com>
2021-09-24 00:40:48 -0500

    Improved driving mechanics.

--- game/scenes/vehicles/ambulance.gd
+++ game/scenes/vehicles/ambulance.gd
@@ -1,30 +1,18 @@
-extends VehicleBody
+extends Vehicle
 
-const STEER_SPEED = 1
-const STEER_LIMIT = 0.6
-var steer_target = 0
-export var engine_force_value = 400
+func _ready():
+	MAX_ENGINE_FORCE = 250
+	STEER_SPEED = 1
+	STEER_LIMIT = 0.6
 
-func _physics_process(delta):
-	var fwd_mps = transform.basis.xform_inv(linear_velocity).x
-	
-	# global_transform = node.global_transform
+func _physics_process(_delta):
 	get_node("ambulance/Siren/Spatial").global_transform = global_transform
+
+func turn_on_siren():
+	pass # create a timer for handling siren lights
 	
-	steer_target = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
-	steer_target *= STEER_LIMIT
+func turn_off_siren():
+	pass # create a timer for handling turning off siren
 	
-	if Input.is_action_pressed("move_back"):
-		engine_force = engine_force_value
-	else:
-		engine_force = 0
-	
-	if Input.is_action_pressed("move_forward"):
-		if (fwd_mps >= -1):
-			engine_force = -engine_force_value
-		else:
-			brake = 1
-	else:
-		brake = 0.0
-	
-	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
+func toggle_mini_mission():
+	pass # some vehicles has mini missions like gta.
--- game/state_machine.gd
+++ game/state_machine.gd
@@ -2,7 +2,6 @@ extends Node
 class_name StateMachine
 
 const DEBUG = true
-
 var state: Object
 
 func _ready():
